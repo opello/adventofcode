a91cd5a9284d5df1ba2ecef4e8c896842b526eb5
Dan Christensen <opello@opello.org>
2015-12-03 23:14:21 -0600

    Solution for day 4 part 2.

---
+++ python/04-2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import hashlib
+
+prefix = ''
+number = 1
+
+with open('../inputs/04.txt') as f:
+   prefix = f.readlines()
+
+prefix = prefix[0].rstrip()
+
+while True:
+   md5 = hashlib.md5()
+   md5.update('{0}{1}'.format(prefix, number))
+
+   if md5.hexdigest()[:6] == '000000':
+      #print md5.hexdigest()
+      print number
+      break
+
+   number += 1
