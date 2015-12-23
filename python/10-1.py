5caf27959ff336e1b4bc2af5d3ad149099fef972
Dan Christensen <opello@opello.org>
2015-12-23 11:52:51 -0600

    Input and solutions for day 10.

---
+++ python/10-1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 
+
+import re
+
+def look_and_say(s):
+   pieces = re.findall(r'((\d)\2*)', s)
+
+   output = ''
+   for piece in pieces:
+      l = len(piece[0])
+      if l > 0:
+         output += str(l)
+         output += str(piece[1])
+
+   return output
+
+with open('../inputs/10.txt') as f:
+   for line in f:
+      line = line.rstrip()
+
+      temp = line
+      for i in range(40):
+         temp = look_and_say(temp)
+      
+      print len(temp)
